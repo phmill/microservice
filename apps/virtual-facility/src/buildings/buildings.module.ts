a894fe50fbce41e4fac0496cd86354696654b094
phmill <ph.mill@gmail.com>
2024-05-05 23:41:34 +0200

    configuring docker compose

--- apps/virtual-facility/src/buildings/buildings.module.ts
+++ apps/virtual-facility/src/buildings/buildings.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { BuildingsService } from './buildings.service';
 import { BuildingsController } from './buildings.controller';
+import { Building } from './entities/building.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Building])], // 👈
   controllers: [BuildingsController],
   providers: [BuildingsService],
 })
--- apps/workflows-service/src/workflows/workflows.module.ts
+++ apps/workflows-service/src/workflows/workflows.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { WorkflowsService } from './workflows.service';
 import { WorkflowsController } from './workflows.controller';
+import { Workflow } from './entities/workflow.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Workflow])], // 👈
   controllers: [WorkflowsController],
   providers: [WorkflowsService],
 })
